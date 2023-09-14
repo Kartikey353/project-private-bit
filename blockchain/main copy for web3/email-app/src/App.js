04ead54533188cf146074b5de8f246958b838c5e
Kartikey Bhardwaj <shyamsharmamohan444@gmail.com>
2023-09-14 21:08:55 +0530

    initial commit

---
+++ blockchain/main copy for web3/email-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import B from './B';
+
+function App() {
+  return (
+    <div className="parent">
+      parent
+      <B />
+    </div>
+  );
+}
+
+export default App;
